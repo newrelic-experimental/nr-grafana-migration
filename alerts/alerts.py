9aa3cdbef9a36637ae9488a8300b6e66f28e69c1
Samuel Vandamme <samuel@sava.be>
2021-04-22 14:40:39 +0200

    chore: added details on work and NR alert format

--- alerts/alerts.py
+++ alerts/alerts.py
@@ -9,7 +9,7 @@ if not os.path.exists('output'):
 def processAlert(alert):
     name = alert['alert']
     expr = alert['expr'].replace('\n', '')
-    severity = 'critical'
+    severity = 'critical'.upper()
     timerange = alert['for'] if 'for' in alert else 5
     message = ""
     runbook_url = ""
@@ -23,16 +23,73 @@ def processAlert(alert):
             runbook_url = alert['annotations']['runbook_url']
 
     print("{%s}" % expr)
+    # Parsing expression
+    # Problems
+    # - The threshold we need for New Relic is contained in the PromQL query, usually in form of >, >=, ==, <, <=
+    # - Function absent needs to be converted to loss of Signal Settings in New Relic
 
     with open("output/%s.yml" % name, 'w') as yamlfile:
+        # This is the New Relic alerts format
         yaml.dump({
+            # Name of the alert
             "name": name,
-            "expr": expr,
-            "severity": severity,
-            "timerange": timerange,
-            "message": message,
-            "runbook_url": runbook_url
-        }, yamlfile, sort_keys = True, default_flow_style=False, width=1000)
+            # Description and details
+            "description": message,
+            # Type of alert
+            "type": "static",
+            # NRQL query
+            "nrql": {
+                "query": expr # Needs to be translated
+            },
+            # Function used to aggregate the NRQL query value(s) for comparison to the terms.threshold (Default: SINGLE_VALUE)
+            "valueFunction": "SINGLE_VALUE", # Options: SINGLE_VALUE | SUM"
+            # List of Critical and Warning thresholds for the condition
+            "terms": [
+                {
+                    "priority": severity,
+                    # Value that triggers a violation
+                    # TODO: Replace with correct value
+                    "threshold": 0,
+                    "thresholdDuration": timerange,
+                    # Operator on the value
+                    # TODO: Replace with correct value
+                    "operator": "ABOVE", # Options: ABOVE | BELOW | EQUALS
+                    # How many data points must be in violation for the duration
+                    "thresholdOccurrences": "ALL", # Options ALL | AT_LEAST_ONCE
+                }
+            ],
+            # Loss of Signal Settings
+            "expiration": {
+                # Close open violations if signal is lost (Default: false)
+                # TODO: Replace with correct value
+                "closeViolationsOnExpiration": "replaceMe", # Options true | false
+                # Open "Loss of Signal" violation if signal is lost (Default: false)
+                # TODO: Replace with correct value
+                "openViolationOnExpiration": True, # Options true | false
+                # Time in seconds; Max value: 172800 (48hrs), null if closeViolationsOnExpiration and openViolationOnExpiration are both 'false'
+                # TODO: Replace with correct value
+                "expirationDuration": 0,
+            },
+
+            # Advanced Signal Settings
+            "signal": {
+                # Duration of the time window used to evaluate the NRQL Condition
+                # Time in seconds; 30 - 900 (Default: 60)
+                # TODO: Replace with correct value
+                "aggregationWindow": 0,
+                # The number of windows we look back at the aggregated data
+                # Max 20 minutes, multiple of aggregationWindow
+                # TODO: Replace with correct value
+                "evaluationOffset": 0,
+                # Type of value that should be used to fill gaps
+                # TODO: Replace with correct value
+                "fillOption": "replaceMe", # Options LAST_VALUE | NONE | STATIC
+                # Integer; Used in conjunction with STATIC fillOption, otherwise null
+                # TODO: Replace with correct value
+                "fillValue": 0,
+            },
+            "runbookUrl": runbook_url
+        }, yamlfile, default_flow_style=False, width=1000)
 
 
 def processRule(rule):
